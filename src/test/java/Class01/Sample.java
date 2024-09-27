ff553e28fad8c70090c27e710d6ddc35f187a9ae
HarshaP10 <pelletiharsha@gamil.com>
2024-09-27 21:01:10 +0530

    first push

---
+++ src/test/java/Class01/Sample.java
@@ -0,0 +1,11 @@
+package Class01;
+
+//class start with caps
+
+public class Sample {
+    public static void main (String[] args)
+    {
+        System.out.println("Hello Java");
+        System.out.println(4*23);
+    }
+}
